4a1bbb12ab9b07301bd16c14ff3189a3f5f93b3e
unknown <natnael.liza@gmail.com>
2020-08-03 23:50:13 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+// viewed at http://localhost:8080
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/home.html'));
+});
+
+app.listen(8080, function () {
+    console.log("app is running...")
+});
